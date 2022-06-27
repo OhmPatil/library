c87c3188433c6f8d549863bcb072aa2587cdbc0a
OhmPatil <workpatil10@gmail.com>
2022-06-28 01:36:22 +0530

    Create functions to add books to array and create cards in DOM dynamically

--- main.js
+++ main.js
@@ -14,8 +14,42 @@ Book.prototype.info = function(){
     else return('read good job')
 }
 
-let book1 = new Book('ohm', 'me', 500, true)
-myLibrary.push(book1)
-console.log(book1.info())
+function addBook(title, author, pages, isRead){
+    let newBook = new Book(title, author, pages, isRead)
+    myLibrary.push(newBook)
+    console.log('Book Added');
+}
 
-console.log(myLibrary[0]);
+addBook('Hobbit', 'me', 200, false)
+addBook('asdasd', 'you', 500, true)
+addBookToDom()
+
+function addBookToDom(){
+    myLibrary.forEach(myBook =>{
+        let card = document.createElement('div')
+        card.classList.add('card')
+
+        let title = document.createElement('div')
+        title.classList.add('title')
+        title.textContent = myBook.title
+        let author = document.createElement('div')
+        author.classList.add('author')
+        author.textContent = myBook.author
+        let pages = document.createElement('div')
+        pages.classList.add('pages')
+        pages.textContent = myBook.pages
+        let isRead = document.createElement('div')
+        isRead.classList.add('isRead')
+        isRead.textContent = myBook.isRead
+        
+        card.appendChild(title)
+        card.appendChild(author)
+        card.appendChild(pages)
+        card.appendChild(isRead)
+
+        
+        document.querySelector('.container').appendChild(card)
+        console.log('Card created');
+
+    })
+}
